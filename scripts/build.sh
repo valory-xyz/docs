80741683bd7909ea93fa6485b5eaae723ffb45dc
8baller <8ball030@gmail.com>
2023-01-13 15:01:07 +0000

    feat/feedback

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
 
-while read -r line; do
-    if [[ $line == *"path"* ]]; then
-        submodule_path=$(echo $line | awk '{print $3}')
-        cd $submodule_path
-		echo "Updating $submodule_path"
-        git pull origin main || exit 1 
-        git checkout $(git tag | tail -n 1)
-        cd ..
-    fi
-done < .gitmodules
+function build-tagged() {
+    while read -r line; do
+        if [[ $line == *"path"* ]]; then
+            submodule_path=$(echo $line | awk '{print $3}')
+            cd $submodule_path
+            echo "Updating $submodule_path"
+            git pull origin main || exit 1
+            git checkout $(git tag | tail -n 1)
+            cd ..
+        fi
+    done < .gitmodules
+}
+
+git submodule update --init --recursive
